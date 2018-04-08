7821c369aa22552ec9514bf36a71ee306a8ff7a2
alex.antonison <alex.antonison@gmail.com>
2018-04-08 15:24:35 -0500

    First working ubuntu run script

--- setup-ec2-instance.sh
+++ setup-ec2-instance.sh
@@ -1,20 +1,48 @@
+# Assumes you have run the following command
+# git clone https://github.com/alex-antonison/setup-cca-project.git
 # Ec2 instance notes
-# Size: m5.2xlarge
+# OS: Ubuntu - ami-e7d9038c
+# Size: m4.2xlarge
 # EBS: 30 GB EBS
-# 
-
-
+# Default security with all ports open
 
 # Updating instance
-sudo yum update
+git clone https://github.com/alex-antonison/setup-cca-project.git
+sudo apt-get update && sudo apt-get upgrade -y
+mkdir local-dev
 
 # Installing and starting Docker
-sudo yum install docker
-sudo service docker start
+sudo apt install docker.io -y
 
 # Setting up hortonworks vm
-wget https://s3.amazonaws.com/antonison-shared-hortonworks-for-project/start-sandbox-hdp-standalone_2-6-4.sh
-chmod 770 start-sandbox-hdp-standalone_2-6-4.sh
-sudo ./start-sandbox-hdp-standalone_2-6-4.sh
+# Works
+chmod 770 setup-cca-project/start-sandbox-hdp-standalone_2-6-4.sh
+sudo ./setup-cca-project/start-sandbox-hdp-standalone_2-6-4.sh
+
+# Setup Python Ubuntu
+sudo apt-get install python3-pip -y 
+sudo pip3 install --upgrade pip
+sudo pip3 install happybase
+sudo pip3 install thrift
+
+# Lets try cloudera
+# Putting on hold for now
+# sudo docker pull cloudera/quickstart:latest
+# sudo docker run --hostname=quickstart.cloudera --privileged=true \
+# -t -i -v /home/ubuntu/local-dev:/local-dev \
+# --publish-all=true \
+# -p 8888 \
+# cloudera/quickstart /usr/bin/docker-quickstart
+
+# Setup Python CDH
+# CDH is being a pain, going back to HDP
+# sudo yum -y update
+# sudo yum -y install yum-utils
+# sudo yum -y groupinstall development
+# sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+# sudo yum -y install python36u
+# sudo yum -y install python36u-pip
+
+
 
 
