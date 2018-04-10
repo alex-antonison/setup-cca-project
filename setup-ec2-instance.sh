cea67877fe1f3519704ff6efc030d8115531ac4d
alex.antonison <alex.antonison@gmail.com>
2018-04-09 22:47:40 -0500

    Added elastic search

--- setup-ec2-instance.sh
+++ setup-ec2-instance.sh
@@ -31,6 +31,41 @@ sudo pip3 install --upgrade pip
 sudo pip3 install happybase
 sudo pip3 install thrift
 
+# Setup Elasticsearch
+
+# Setup neo4j
+## Presets up so you do not have to acknowledge anything
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+sudo debconf-set-selections
+
+## Setup Java
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+
+## Setup neo4j
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+
+## Had to go back to 3.0 to get a working version
+sudo docker run -d --restart unless-stopped \
+    --publish=7474:7474 --publish=7687:7687 \
+    --volume=$HOME/neo4j/data:/data \
+    --volume=$HOME/neo4j/logs:/logs \
+    neo4j:3.0
+
+# Setup Elasticsearch
+
+## Install docker compose
+sudo pip install docker-compose
+
+## Compose up elastic search
+cd elastic-search-compose
+docker-compose up
+
 # Setting up hortonworks vm
 chmod 770 setup-cca-project/start-sandbox-hdp-standalone_2-6-4.sh
 sudo ./setup-cca-project/start-sandbox-hdp-standalone_2-6-4.sh
